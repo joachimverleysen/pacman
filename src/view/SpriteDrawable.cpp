8b56075a3bf3e09bb1eb6005b11932888207e012
Joachim Verleysen <jkverleysen@gmail.com>
2025-11-03 20:59:59 +0100

    implemented animations for pacman & ghosts

--- src/view/SpriteDrawable.cpp
+++ src/view/SpriteDrawable.cpp
@@ -1,12 +1,13 @@
 #include "SpriteDrawable.h"
 #include "../configure/constants.h"
 #include "Visuals.h"
+#include "../logic/utils/Stopwatch.h"
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Texture.hpp>
 
 SpriteDrawable::SpriteDrawable(Texture::TextureMap texture_map, float scale)
     : texture_map_(texture_map), scale_(scale),
-      current_texture_(texture_map_[Entity::State::IDLE]) {
+      current_texture_(texture_map_[Entity::State::IDLE][0]) {
   sprite_.setTexture(*current_texture_);
   sprite_.setScale(scale, scale);
   sf::FloatRect bounds = sprite_.getLocalBounds();
@@ -19,11 +20,27 @@ void SpriteDrawable::setPosition(MyVector pos) {
 }
 
 void SpriteDrawable::setTexture(const sf::Texture *texture) {
+  current_texture_ = texture;
   sprite_.setTexture(*texture);
 }
 
 void SpriteDrawable::updateState(Entity::State state) {
+  if (state != state_)  // Animation switch
+    texture_index_ = 0;
   state_ = state;
-  const sf::Texture *texture_new = texture_map_[state_];
+  const sf::Texture *texture_new = texture_map_[state_][texture_index_];
   setTexture(texture_new);
+  accumulated_delta_time_ += Stopwatch::getInstance()->getDeltaTime();
+  if (accumulated_delta_time_ * 500 > animation_interval_) {
+    accumulated_delta_time_ = 0;
+    nextTexture();
+  }
+
+}
+
+
+void SpriteDrawable::nextTexture() {
+  auto textures = texture_map_[state_];
+  texture_index_ = (texture_index_ + 1) % textures.size();
+  setTexture(textures[texture_index_]);
 }
